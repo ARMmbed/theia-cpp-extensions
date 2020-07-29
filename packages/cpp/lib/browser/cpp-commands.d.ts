6cf3c390bf912c874efa39c602f811d81a81847c
Nigel Westbury <nigelipse@miegel.org>
2020-07-29 15:21:31 +0100

    HACK - Commit lib directory

---
+++ packages/cpp/lib/browser/cpp-commands.d.ts
@@ -0,0 +1,57 @@
+/********************************************************************************
+ * Copyright (C) 2017 TypeFox and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+import { SelectionService } from '@theia/core/lib/common';
+import { CommandContribution, CommandRegistry, Command } from '@theia/core/lib/common';
+import { OpenerService } from '@theia/core/lib/browser';
+import { CppLanguageClientContribution } from './cpp-language-client-contribution';
+import { EditorManager } from '@theia/editor/lib/browser';
+/**
+ * Command to switch between source/header files.
+ */
+export declare const SWITCH_SOURCE_HEADER: Command;
+/**
+ * Command that is used to show the references from a CodeLens.
+ */
+export declare const SHOW_CLANGD_REFERENCES: Command;
+/**
+ * Command to open the file path.
+ * @param path the file path.
+ */
+export declare const FILE_OPEN_PATH: (path: string) => Command;
+/**
+ * Determine if a C/C++ file is currently active (opened).
+ * @param editorManager the editor manager if it is defined.
+ *
+ * @returns `true` if the current active editor is a C/C++ file.
+ */
+export declare function editorContainsCppFiles(editorManager: EditorManager | undefined): boolean;
+export declare class CppCommandContribution implements CommandContribution {
+    protected readonly clientContribution: CppLanguageClientContribution;
+    protected readonly openerService: OpenerService;
+    private editorService;
+    protected readonly selectionService: SelectionService;
+    constructor(clientContribution: CppLanguageClientContribution, openerService: OpenerService, editorService: EditorManager, selectionService: SelectionService);
+    /**
+     * Register commands for C/C++.
+     * @param commands the command registry.
+     */
+    registerCommands(commands: CommandRegistry): void;
+    /**
+     * Actually switch the source header.
+     */
+    protected switchSourceHeader(): Promise<void>;
+}
+//# sourceMappingURL=cpp-commands.d.ts.map
---
+++ packages/cpp/lib/browser/cpp-preferences.js
@@ -0,0 +1,110 @@
+"use strict";
+/********************************************************************************
+ * Copyright (C) 2018 Ericsson and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+Object.defineProperty(exports, "__esModule", { value: true });
+var preferences_1 = require("@theia/core/lib/browser/preferences");
+var common_1 = require("../common");
+/**
+ * Representation of the C/C++ preference schema.
+ */
+exports.cppPreferencesSchema = {
+    type: 'object',
+    properties: {
+        'cpp.buildConfigurations': {
+            description: 'List of build configurations',
+            type: 'array',
+            items: {
+                type: 'object',
+                properties: {
+                    'name': {
+                        type: 'string'
+                    },
+                    'directory': {
+                        type: 'string'
+                    },
+                    'commands': {
+                        type: 'object',
+                        properties: {
+                            'build': {
+                                type: 'string',
+                            }
+                        },
+                    }
+                },
+                required: ['name', 'directory'],
+            },
+            scope: 'resource',
+            default: [],
+        },
+        'cpp.experimentalCompilationDatabaseMap': {
+            description: 'Enable experimental support for multiple compilation databases.',
+            default: false,
+            type: 'boolean'
+        },
+        'cpp.clangdExecutable': {
+            description: 'Specify the executable name/path to run in order to start clangd.',
+            default: common_1.CLANGD_EXECUTABLE_DEFAULT,
+            type: 'string'
+        },
+        'cpp.clangdArgs': {
+            description: 'Specify the arguments to pass to clangd when starting the language server.',
+            default: '',
+            type: 'string'
+        },
+        'cpp.clangTidy': {
+            description: 'Enable/disable C/C++ linting.',
+            default: false,
+            type: 'boolean'
+        },
+        'cpp.clangTidyChecks': {
+            description: 'Specify comma separated arguments to pass to clang-tidy. Activated only if cpp.clang-tidy is enabled',
+            default: '',
+            type: 'string'
+        }
+    }
+};
+/**
+ * Representation of C/C++ preference configuration.
+ */
+var CppConfiguration = /** @class */ (function () {
+    function CppConfiguration() {
+    }
+    return CppConfiguration;
+}());
+exports.CppConfiguration = CppConfiguration;
+exports.CppPreferences = Symbol('CppPreferences');
+/**
+ * Create the C/C++ preferences.
+ * @param preferences the preference service.
+ *
+ * @returns the C/C++ preferences.
+ */
+function createCppPreferences(preferences) {
+    return preferences_1.createPreferenceProxy(preferences, exports.cppPreferencesSchema);
+}
+exports.createCppPreferences = createCppPreferences;
+/**
+ * Bind the C/C++ preferences to the application.
+ */
+function bindCppPreferences(bind) {
+    bind(exports.CppPreferences).toDynamicValue(function (ctx) {
+        var preferences = ctx.container.get(preferences_1.PreferenceService);
+        return createCppPreferences(preferences);
+    }).inSingletonScope();
+    bind(preferences_1.PreferenceContribution).toConstantValue({ schema: exports.cppPreferencesSchema });
+}
+exports.bindCppPreferences = bindCppPreferences;
+//# sourceMappingURL=cpp-preferences.js.map
